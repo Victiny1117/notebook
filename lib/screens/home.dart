4a9b31ae46f8197b444850d1627b09597b079e4a
Victiny1117 <146040261+Victiny1117@users.noreply.github.com>
2025-04-03 17:03:55 -0400

    hice el archivo para manejar notas e hice que actualice la lista de notas al iniciar la app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import '../services/services.dart';
 
 class HomeScreen extends StatefulWidget {
 
   @override
-  
+
   _HomeScreenState createState() => _HomeScreenState();
+
 }
 
 class _HomeScreenState extends State<HomeScreen> {
 
-@override
-
+  List<File> notes = []; //lista de archivos de notas
+  String search = "";
+  Services serv = Services();
+  
+  @override 
+    void initState() {
+      super.initState();
+      loadNotes();
+    }
+    Future<void> loadNotes() async {
+      final files = await serv.getNotesFiles();
+      notes = files;
+    }
+    
   Widget build(BuildContext context) {
     return Scaffold (
 
---
+++ lib/services/services.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class Services {
+
+  Future<Directory> getNotesFolder() async { //obtener carpeta
+    final directory = await getApplicationDocumentsDirectory();
+    final notesFolder = Directory('${directory.path}/notes');
+
+    if (!await notesFolder.exists()) {
+      await notesFolder.create();
+    }
+    return notesFolder;
+  }
+
+  Future<void> createNote(String content, String name) async { //crear notas
+    final folder = await getNotesFolder();
+    final noteFile = File('${folder.path}/$name.text');
+    await noteFile.writeAsString(content);
+  }
+
+  Future<List<File>> getNotesFiles() async { //obtener archivos
+    final folder = await getNotesFolder();
+    return folder.listSync().whereType<File>().toList();
+  }
+
+  Future<void> updateNote(File note, String newContent) async { //editar notas
+    await note.writeAsString(newContent);
+  }
+
+}
